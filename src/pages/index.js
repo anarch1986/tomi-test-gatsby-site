91d883e783a7aa341484f0e4890ee4a1c604a339
tamas.varga5@vodafone.com <tamas.varga5@vodafone.com>
2022-03-06 16:20:00 +0100

    Added GraphQL functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+
+import Layout from "../components/layout";
 
 function IndexPage() {
   return (
     <Layout pageTitle="Home Page">
-      <h3>Welcome to Tomi's Test Gatsby site!</h3>
+      <h3>Welcome to Tomi's Test Gatsby Site!</h3>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="New annihilation stage after March 7."
